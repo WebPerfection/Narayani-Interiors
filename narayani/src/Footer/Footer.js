5737fd59e8a5e528e1fd65b7055059e5bdf8a265
Awresh <experiments.worlds@gmail.com>
2023-05-07 18:07:30 +0530

    Footer

--- narayani/src/Footer/Footer.js
+++ narayani/src/Footer/Footer.js
@@ -1,7 +1,54 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import Image1 from '../ImageData/footer-img/gallery-2.jpg';
+import Image2 from '../ImageData/footer-img/gallery-3.jpg';
+import Image3 from '../ImageData/footer-img/gallery-4.jpg';
+import logo from '../Navbar/web.png';
+import './Footer.css';
+
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="logo-container">
+          <img src={logo}alt="Awesome Logo" className="logo" />
+          <p className="address">Flat 20, Reynolds Neck, North Helenaville, FV77 8WS</p>
+        </div>
+        <div className="contact-container">
+          <p className="phone">PHONE: +324 123 45 978 & 01</p>
+          <p className="hours">Mon - Friday: 9.00am to 6.00pm</p>
+          <p className="email">EMAIL: abc@yourdomain.com</p>
+          <p className="email">crystalocareer@gmail.com</p>
+        </div>
+        <div className="services-container">
+          <h3>Services</h3>
+          <ul>
+            <li>Concept Design</li>
+            <li>Project Designs</li>
+            <li>Make Overs</li>
+            <li>Consulting</li>
+            <li>Glass & Wrought</li>
+            <li>Space Planning</li>
+          </ul>
+        </div>
+        <div className="news-container">
+          <h3>Recent News</h3>
+          <div className="news-item">
+            <img src={Image1} alt="Awesome Image" />
+            <p>March 10, 2019</p>
+            <p>Creating drama and feeling with...</p>
+          </div>
+          <div className="news-item">
+            <img src={Image2} alt="Awesome Image" />
+            <p>March 02, 2019</p>
+            <p>Wondering if interior design is dying...</p>
+          </div>
+          <div className="news-item">
+            <img src={Image3} alt="Awesome Image" />
+            <p>February 27, 2019</p>
+            <p>Enjoy monsoon in comfort of your...</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
