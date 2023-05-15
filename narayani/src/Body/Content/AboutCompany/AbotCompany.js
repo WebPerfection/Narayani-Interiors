ff6a4cf2ce2679c2af7ce98502b5d0045907b726
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-05-15 20:35:37 +0530

    footer_work

--- narayani/src/Body/Content/AboutCompany/AbotCompany.js
+++ narayani/src/Body/Content/AboutCompany/AbotCompany.js
@@ -18,7 +18,7 @@ export default function AbotCompany() {
           <h6>About Us</h6>
           <h1>Experience the Essence of Exceptional Interior Design</h1>
           <p>
-            At our company, we are a dedicated and highly efficient team of interior designers. We take pride in creating captivating spaces that leave a lasting impression. Our passion for design knows no bounds, and we are committed to delivering extraordinary results.
+            {/* At our company, we are a dedicated and highly efficient team of interior designers. We take pride in creating captivating spaces that leave a lasting impression. Our passion for design knows no bounds, and we are committed to delivering extraordinary results.
             <br/>
             
 
@@ -28,7 +28,8 @@ export default function AbotCompany() {
             <br/>
             We understand that each project is unique, and we tailor our services to fit your specific vision and requirements. Our meticulous attention to detail ensures that every aspect of your project is handled with utmost care and precision.
             <br/>
-            Join us on this extraordinary design adventure, and let us bring your interior design dreams to life. Together, we will create spaces that inspire, delight, and surpass your wildest imagination.
+            Join us on this extraordinary design adventure, and let us bring your interior design dreams to life. Together, we will create spaces that inspire, delight, and surpass your wildest imagination. */}
+            At our company, we are a dedicated team of interior designers committed to delivering extraordinary results. With a deep understanding of our clients' needs, we transform spaces into personalized works of art, paying meticulous attention to every detail from colors to furniture placement. We denounce the ordinary and embrace innovation and creativity in all aspects of our work, creating harmonious environments that bring joy and tranquility. Tailoring our services to your unique vision, we handle each project with utmost care and precision. Join us on this design adventure to bring your dreams to life. Together, we'll create inspiring spaces that surpass your imagination.
           </p>
         </div>
       </div>
--- narayani/src/Footer/Footer.js
+++ narayani/src/Footer/Footer.js
@@ -9,80 +9,79 @@ import '../Navbar/Navbar.css'
 export default function Footer() {
   return (
     <footer>
-    <div class="row primary">
-      <div class="column about">
-        <h3 class="footer_h3">Foolish Developer</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-          voluptatem corporis error non,
-        </p>
-                  <div class="social">
-                    <i class="fa-brands fa-facebook-square"></i>
-                    <i class="fa-brands fa-instagram-square"></i>
-                    <i class="fa-brands fa-twitter-square"></i>
-        <i class="fa-brands fa-youtube-square"></i>
-          <i class="fa-brands fa-whatsapp-square"></i>
-                  </div>
-      </div>
-      <div class="column links">
-        <h3 class="footer_h3">Some Links</h3>
-        <ul className='footer_ul'>
-          <li>
-            <a href="#faq">F.A.Q</a>
-          </li>
-          <li>
-            <a href="#cookies-policy">Cookies Policy</a>
-          </li>
-          <li>
-            <a href="#terms-of-services">Terms Of Service</a>
-          </li>
-          <li>
-            <a href="#support">Support</a>
-          </li>
-
-        </ul>
-      </div>
-        <div class="column links">
-          <h3 class="footer_h3">Some Links</h3>
-          <ul className='footer_ul'>
-            <li>
-              <a href="#faq">F.A.Q</a>
-            </li>
-            <li>
-              <a href="#cookies-policy">Cookies Policy</a>
-            </li>
-            <li>
-              <a href="#terms-of-services">Terms Of Service</a>
-            </li>
-            <li>
-              <a href="#support">Support</a>
-            </li>
-
-          </ul>
+      <div class="row primary">
+        <div class="column about">
+          <h3 class="footer_h3">Foolish Developer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            voluptatem corporis error non,
+          </p>
+          <div class="social">
+            <i class="fa-brands fa-facebook-square"></i>
+            <i class="fa-brands fa-instagram-square"></i>
+            <i class="fa-brands fa-twitter-square"></i>
+            <i class="fa-brands fa-youtube-square"></i>
+            <i class="fa-brands fa-whatsapp-square"></i>
+          </div>
         </div>
-      <div class="column subscribe">
-        <h3 class="footer_h3">Newsletter</h3>
-        <div>
-          <input className='footer_input' type="email" placeholder="Your email id here" />
-          <button className='footer_bt'>Subscribe</button>
+        <div className='Flex'>
+          <div class="column links">
+            <h3 class="footer_h3">Some Links</h3>
+            <ul className='footer_ul'>
+              <li>
+                <a href="#faq">F.A.Q</a>
+              </li>
+              <li>
+                <a href="#cookies-policy">Cookies Policy</a>
+              </li>
+              <li>
+                <a href="#terms-of-services">Terms Of Service</a>
+              </li>
+              <li>
+                <a href="#support">Support</a>
+              </li>
+
+            </ul>
+          </div>
+          <div class="column links">
+            <h3 class="footer_h3">Some Links</h3>
+            <ul className='footer_ul'>
+              <li>
+                <a href="#faq">F.A.Q</a>
+              </li>
+              <li>
+                <a href="#cookies-policy">Cookies Policy</a>
+              </li>
+              <li>
+                <a href="#terms-of-services">Terms Of Service</a>
+              </li>
+              <li>
+                <a href="#support">Support</a>
+              </li>
+
+            </ul>
+          </div>
         </div>
+        <div class="column subscribe">
+          <h3 class="footer_h3">Newsletter</h3>
+          <div>
+            <input className='footer_input' type="email" placeholder="Your email id here" />
+            <button className='footer_bt'>Subscribe</button>
+          </div>
 
+        </div>
       </div>
-    </div>
 
-    <div class="row copyright">
-       <div class="footer-menu">
-
-                     <a href="">Home</a>
-                     <a href="">About</a>
-                     <a href="">Contact</a>
-                     <a href="">Blog</a>
-   <a href="">Social</a>
-
-
-                  </div>
-      <p>Copyright &copy; 2021 Foolish Developer</p>
-    </div>
-  </footer>
+      <div class="row copyright">
+        <div class="footer-menu">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Blog</a>
+          <a href="">Social</a>
+        </div>
+        <p>Copyright &copy; 2021 Foolish Developer</p>
+      </div>
+    </footer>
   );
 }
