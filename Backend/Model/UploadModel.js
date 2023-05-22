8a1cfab94ad4770def834efd3b90993ff3aad718
mohdashiq18 <moashiq2018@gmail.com>
2023-05-22 13:37:36 +0530

    Backend

--- Backend/Model/UploadModel.js
+++ Backend/Model/UploadModel.js
@@ -1,14 +1,18 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const uploadSchema=mongoose.Schema({
-    title:{type:String,required:true},
-    images:[{type:String,required:true}],
-    description:{type:String,required:true},
-    date:{type:String,required:true}
-    
-})
+const uploadSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  images: [{ type: String, required: true }],
+  description: { type: String, required: true },
+  date: { type: String, required: true },
+  category: { type: String, required: true },
+  size: {
+    length: { type: Number, required: true },
+    width: { type: Number, required: true },
+  },
+});
 
-const UploadModel=mongoose.model("Images",uploadSchema)
-module.exports={
-    UploadModel
-}
+const UploadModel = mongoose.model("Images", uploadSchema);
+module.exports = {
+  UploadModel,
+};
--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,57 @@ app.use(
     origin: "*",
   })
 );
+app.get("/getdata",async(req,res)=>{
+  try{
+     const data = await UploadModel.find()
+     res.send(data)
+  }
+  catch{
+    res.send("Err Get Section")
+  }
+})
+app.get("/getdata/:id",async(req,res)=>{
+  const id=req.params.id
+  try{
+     const data = await UploadModel.findOne({"_id":id})
+     res.send(data)
+  }
+  catch{
+    res.send("Err Get Section")
+  }
+})
+app.delete("/delete/:id",async(req,res)=>{
+  const id = req.params.id
+  try{
+      await UploadModel.findByIdAndDelete({"_id":id})
+      res.send("Delete success")
+  }
+  catch{
+     res.send("err Delete section")
+  }
+})
+app.patch("/update/:id",async(req,res)=>{
+  const id = req.params.id
+  const payload=req.body
+  try{
+      await UploadModel.findByIdAndUpdate({"_id":id},payload)
+      res.send("Update success")
+  }
+  catch{
+     res.send("err Update section")
+  }
+})
 app.post("/upload",async(req, res,next) => {
 const payload=req.body
+const currentDate = new Date();
+
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1; 
+const year = currentDate.getFullYear();
+
+const formattedDate = `${day}/${month}/${year}`;
 try{
-   const data=new UploadModel(payload)
+   const data=new UploadModel({...payload,date:formattedDate})
    await data.save()
    res.send(data)
 }
