3eb80899030b7e215f5b2b19bd3f3cc7975adf26
Mohd Awresh <107464990+Awresh@users.noreply.github.com>
2023-07-16 20:28:49 +0530

    add some aditional

--- narayani/src/Body/WorkingProcess/WorkingProcess.js
+++ narayani/src/Body/WorkingProcess/WorkingProcess.js
@@ -14,6 +14,7 @@ import DesignServicesIcon from '@mui/icons-material/DesignServices';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 import { StyledEngineProvider } from '@mui/material/styles';
+import './Working.css'
 
 const QontoConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
